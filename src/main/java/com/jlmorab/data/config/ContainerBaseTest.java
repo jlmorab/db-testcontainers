b5566d740f1bf0cd9329c59cb70f270c67837283
José Luis Mora Barrios <jlmorab@hotmail.com>
2025-04-18 04:13:35 -0600

    Common container

---
+++ src/main/java/com/jlmorab/data/config/ContainerBaseTest.java
@@ -0,0 +1,29 @@
+package com.jlmorab.data.config;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SuppressWarnings("resource")
+public abstract class ContainerBaseTest {
+	
+	private ContainerBaseTest() {}
+
+	static final PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:16")
+			.withDatabaseName("testdb")
+			.withUsername("test")
+			.withPassword("test");
+	
+	static {
+		database.start();
+	}
+	
+	@DynamicPropertySource
+    static void databaseProperties( DynamicPropertyRegistry registry ) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.datasource.password", database::getPassword);
+        registry.add("spring.datasource.driver-class-name", database::getDriverClassName);
+    }//end databaseProperties()
+	
+}
